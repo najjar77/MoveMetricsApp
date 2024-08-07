b0e424aa35569dbda13d34a2bbad19432f17c6be
najjar77 <najjar.amiin@gmail.com>
2024-08-07 11:23:57 +0200

    chore: Update dependencies for Firebase authentication and Google Sign-In

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MainApp());
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Move Metrics App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
     );
   }
